b46504d5e0d765c3aa81da2d4b4364149c7015fd
NickM69 <93398980+NickM69@users.noreply.github.com>
2024-09-13 15:48:48 -0400

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,1 +1,14 @@
 # Placeholder for detail.php
+<?php
+inlcude('index.php');
+$index = $_GET['index'];
+?>
+<head>
+  <title>Blog World</title>
+</head>
+<body>
+  <h1><?= $blog[$index]['title'] ?> </h1>
+  <h2><?= $blog[$index]['content']?> </h2>
+  <p><?= $blog[$index]['author'] ?></p>
+  <p><?= $blog[$index]['date'] ?></p>
+</body>
