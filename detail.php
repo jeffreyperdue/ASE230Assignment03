43b966636eb181140a094a2428145f6d39848ae2
NickM69 <93398980+NickM69@users.noreply.github.com>
2024-09-22 16:28:28 -0400

    added code to my branch

--- detail.php
+++ detail.php
@@ -75,6 +75,35 @@ $blog=[
     ]
 ];
 $index = $_GET['index'];
+
+if (!file_exists('visitors.csv')){
+        $fp = fopen('visitors.csv', 'w');
+        fputcsv($fp, array('post_id', 'visits'));
+        fclose($fp);
+    }
+
+    $post_id = $blog[$index]['title'];
+    $visits = 1;
+    if (file_exists('visitors.csv')) {
+    $fp = fopen('visitors.csv', 'r+');
+    $rows = array();
+    while (($row = fgetcsv($fp)) !== FALSE) {
+        if ($row[0] == $post_id) {
+            $row[1] = (int) $row[1] + 1; 
+        }
+        $rows[] = $row;
+    }
+        fseek($fp, 0);
+        foreach ($rows as $row) {
+            fputcsv($fp, $row);
+        }
+        fclose($fp);
+    }
+    else{
+        $fp = fopen('visitors.csv', 'w');
+        fputcsv($fp, array($post_id, $visits));
+        fclose($fp);
+    }
 ?>
 <head>
   <title>Blog World</title>
